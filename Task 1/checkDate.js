2c577b5892031308180211b64c0679d7e67dc2dc
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-04-02 14:46:42 +0300

    refactor: optimize code

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,28 +1,35 @@
 function checkDate(timestamp) {
-    var day = new Date(timestamp * 1000).getDate();
-    var month = new Date(timestamp * 1000).getMonth();
-    var year = new Date(timestamp * 1000).getFullYear();
-    var hour = new Date(timestamp * 1000).getHours();
+    // var day = new Date(timestamp * 1000).getDate();
+    // var month = new Date(timestamp * 1000).getMonth() + 1; //Месяцы исчисляются от 0 до 11, в изначальном варианте (без +1) месяц был бы некорректным.
+    // var year = new Date(timestamp * 1000).getFullYear();
+    // var hour = new Date(timestamp * 1000).getHours();
 
-    const current_Date = new Date(Date.now());
-    const current_day = current_Date.getDate();
-    const current_month = current_Date.getMonth() + 1;
-    const currentYear = current_Date.getFullYear();
+    // const current_Date = new Date(Date.now()); // Нет необходимости указывать внутри Date.now()
+    // const current_day = current_Date.getDate();
+    // const current_month = current_Date.getMonth() + 1;
+    // const currentYear = current_Date.getFullYear();
 
-    let isSameDate = false;
+    // let isSameDate = false;
 
-    if (year == currentYear) {
-        if (month == current_month) {
-            if (day == current_day) {
-                isSameDate = true;
-            } else {
-                isSameDate = false;
-            }
-        }
-    }
+    // if (year == currentYear) {
+    //     if (month == current_month) {
+    //         if (day == current_day) {
+    //             isSameDate = true;
+    //         } else {
+    //             isSameDate = false;
+    //         }
+    //     }
+    // }
+
+    const today = new Date();
+    const isSameDate = new Date(timestamp * 1000).getDate() == today.getDate() && 
+                      new Date(timestamp * 1000).getMonth() == today.getMonth() &&
+                      new Date(timestamp * 1000).getFullYear() == today.getFullYear();
+    
+    const hours = new Date(timestamp * 1000).getHours();
 
     return {
         isSameDate: isSameDate,
-        dayPeriod: hour > 11 ? 'pm' : 'am'
+        dayPeriod: hours > 11 ? 'pm' : 'am'
     }
 }
